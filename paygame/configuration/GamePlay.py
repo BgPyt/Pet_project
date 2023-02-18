3f369bdca3c88cea795a621e70e216e7f3083a28
BgPyt <malikov.b@yandex.ru>
2023-02-18 13:32:03 +0700

    Tikit_01

--- paygame/configuration/GamePlay.py
+++ paygame/configuration/GamePlay.py
@@ -1,5 +1,10 @@
-from game_to import *
+from paygame.configuration.game_to import Ship, Label
 import tkinter.messagebox as mb
+from tkinter import Button
+import time
+
+first_time = None
+game_time = None
 
 
 class MyButton(Button):
@@ -9,7 +14,8 @@ class MyButton(Button):
 
     def __init__(self, master, x, y, player, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
-        self.config(command=lambda x=self.__class__: self.click(x))
+        self.master = master
+        self.config(command=lambda cls=self.__class__: self.click(cls))
         self.x = x
         self.y = y
         self.player = player
@@ -20,18 +26,57 @@ class MyButton(Button):
     def click(self, cls):
         if cls.init_pole_human > 0:
             self.separation_human(cls)
+        if cls.init_pole_human == 0 and self.player[0] == 'bot':
+            self.damage_human()
 
-    def call_error(self):
+    def damage_human(self):
+        if self.is_ship:
+            ship = self.ship
+            ship.count -= 1
+            self.player[1].counter_ships -= 1
+            self.shoot_down = True
+            self.config(bg=ship.color)
+            self['state'] = 'disable'
+            self.player[1].game_state(self.__class__)
+            if ship.count == 0:
+                for vd in ship.bt_lst:
+                    vd.config(text='☠', bg='red')
+
+        else:
+            self['state'] = 'disable'
+            self.config(text='⊗', fg='#000000')
+            self.player[1].damage_bot(self.__class__)
+
+    def __call__(self, *args, **kwargs):
         mb.showwarning('Невозможно', 'Нужно ставить в один ряд за следующим')
 
     def location(self):
+        """Проверка, что последующий клик будет по рядом по горизонтали или вертикали относительно первого нажатия
+        выставленной кнопки """
         if (self.x, self.y) in [(self.prev.x + 1, self.prev.y), (self.prev.x - 1, self.prev.y),
                                 (self.prev.x, self.prev.y + 1), (self.prev.x, self.prev.y - 1)]:
             return True
 
+        else:
+            self()
+
     def is_validator(self, deck=None):
+        """Проверка валидации и конфигарция виджетов на то, что они не выходят за границу и нету ли занимаемым им
+        полем других кораблей """
         lst = self.player[1].buttons
         x, y = self.x, self.y
+        if self.init_pole_human > 10 and self.prev is None:
+            if x <= 8 and not lst[x + 1][y].is_ship:
+                return True
+            if x >= 1 and not lst[x - 1][y].is_ship:
+                return True
+            if y <= 8 and not lst[x][y + 1].is_ship:
+                return True
+            if y >= 1 and not lst[x][y - 1].is_ship:
+                return True
+            mb.showwarning('Пользовательская ошибка', 'Нельзя тут выставить')
+            return False
+
         if self.prev is None and deck:
             if x <= 6 and not lst[x + 1][y].is_ship and not lst[x + 2][y].is_ship and not lst[x + 3][y].is_ship:
                 return True
@@ -70,7 +115,6 @@ class MyButton(Button):
                         return True
                     if 'x' in t and not lst[select_x][y].is_ship:
                         bt = lst[select_x][y]
-                        print(t[1] - (self.prev.x - x))
                         bt.ship = self.prev.ship
                         bt.is_ship = True
                         bt.ship.bt_lst.append(bt)
@@ -81,9 +125,9 @@ class MyButton(Button):
 
             if deck and self.location():
                 t = ('y', y) if x == self.prev.x else ('x', x)
-                if 2 <= t[1] <= 7:
-                    select_y = t[1] - (self.prev.y - y)
-                    select_x = t[1] - (self.prev.x - x)
+                select_y = t[1] - (self.prev.y - y)
+                select_x = t[1] - (self.prev.x - x)
+                if (select_y > 0 and t[0] == 'y') or (select_x > 0 and t[0] == 'x'):
                     if 'y' in t and not lst[x][select_y].is_ship and not lst[x][select_y - (self.prev.y - y)].is_ship:
                         for bt in (lst[x][select_y], lst[x][select_y - (self.prev.y - y)]):
                             bt.ship = self.prev.ship
@@ -100,20 +144,18 @@ class MyButton(Button):
                         return True
                     else:
                         return False
-                else:
-                    return False
 
     def separation_human(self, cls):
         if self.player[0] == 'human' and self.ship is None:
             self.message_player(cls)
             if cls.init_pole_human > 16:
-                self.ship = Ship('single_deck', 1, 'red')
+                self.ship = Ship('single_deck', 1, '#8B4513')
                 cls.init_pole_human = cls.init_pole_human - 1
                 self.is_ship = True
                 self.ship.bt_lst.append(self)
-                self.config(bg='red')
+                self.config(bg='#8B4513')
             elif cls.init_pole_human > 10:
-                if not cls.prev:
+                if not cls.prev and self.is_validator():
                     self.ship = Ship('double_deck', 2, 'orange')
                     self.is_ship = True
                     self.ship.bt_lst.append(self)
@@ -127,8 +169,6 @@ class MyButton(Button):
                     self.config(bg='orange')
                     cls.init_pole_human = cls.init_pole_human - 1
                     cls.prev = None
-                else:
-                    self.call_error()
             elif cls.init_pole_human > 4 and self.is_validator():
                 if not self.prev:
                     self.ship = Ship('three_deck', 3, 'violet')
@@ -144,7 +184,8 @@ class MyButton(Button):
                     self.config(bg='violet')
                     cls.init_pole_human = cls.init_pole_human - 2
                     cls.prev = None
-            elif cls.init_pole_human > 1 and self.is_validator(True):
+                    self.message_player(cls)
+            elif cls.init_pole_human <= 4 and self.is_validator(True):
                 if not self.prev:
                     self.ship = Ship('four_deck', 4, 'black')
                     self.is_ship = True
@@ -159,25 +200,43 @@ class MyButton(Button):
                     self.config(bg='black')
                     cls.init_pole_human = cls.init_pole_human - 2
                     cls.prev = None
+                    self.message_player(cls)
         else:
             mb.showwarning('Предупреждение', 'Что-то пошло ни так')
 
     def message_player(self, cls):
         if cls.init_pole_human == 17:
-            Message_window.place_forget()
-            cls.Message_window = Label(self.master, text='Выставите три двухпалубных корабля', font=('Bauhaus 93', 18))
+            self.player[1].message_window.place_forget()
+            cls.Message_window = Label(self.master, text='Выставите три двухпалубных корабля', foreground="#B71C1C",
+                                       background="#FFCDD2", font=('Bauhaus 93', 18))
             cls.Message_window.place(x=0, y=0)
 
         elif cls.init_pole_human == 11:
-            cls.Message_window.place_forget()
             cls.Message_window.config(text='Выставите два трехпалубных корабля')
 
+        elif cls.init_pole_human == 4:
+            cls.Message_window.config(text='Выставите один четырехпалубный корабль')
 
-        elif cls.init_pole_human == 6:
-            pass
+        elif cls.init_pole_human == 0:
+            cls.Message_window.place_forget()
+            cls.Message_window = Label(self.master, text='Игра началась', foreground="#1E90FF",
+                                       background="#FFEBCD", font=('TkTooltipFont:', 18))
+            cls.Message_window.place(x=400, y=0)
+            self.movement_time(cls)
 
+    def movement_time(self, cls):
+        start_time = time.time()
+        global first_time
+        if first_time is None:
+            first_time = time.time()
+        global game_time
+        game_time = time.gmtime(start_time - first_time)
+        if cls.Message_window is None:
+            first_time = None
+            game_time = None
+        current_timer = f'{str(game_time.tm_min).rjust(2, "0")}:{str(game_time.tm_sec).rjust(2, "0")}'
+        self.master.after(1000, lambda clap=cls: self.movement_time(clap))
+        cls.Message_window.config(text=f'ИГРА НАЧАЛАСЬ\n{current_timer}')
 
-
-
-    def __repr__(self):
-        return f'Button({self.ship},{self.x}, {self.y})'
+    def __str__(self):
+        return f'{self.x} {self.y}'
--- paygame/configuration/game_to.py
+++ paygame/configuration/game_to.py
@@ -1,6 +1,6 @@
-from tkinter import Tk, Label, Button, PhotoImage
-import random
 from GamePlay import *
+from tkinter import Tk, Label, PhotoImage
+from random import randint, choice
 
 
 class Ship:
@@ -9,7 +9,9 @@ class Ship:
         self.name = name
         self.count = count
         self.color = color
-        self.bt_lst = []
+        self.bt_lst = []  # список виджетов
+        self.attempts = set()  # множество ходов
+        self.lst_button = []  # Список всех найденных кораблей
 
     def __repr__(self):
         return f'{self.name, self.count}'
@@ -18,17 +20,242 @@ class Ship:
 class Game_pole:
     row = 10
     column = 10
+    players = []
+    button = None  # значение ссылки на виджет, предоставляемый последний искомый
+    unfinished = []
+    lb = None
+    Message_window = None
+
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        cls.players.append(obj)
+        return obj
 
     def __init__(self, tk, x, y, player):
-        self.buttons = [[MyButton(tk, j, i, (player, self), width=3, bg='#33f') for i in range(self.column)] for j in
+        self.buttons = [[MyButton(tk, j, i, (player, self), width=3, bg='#B0E0E6') for i in range(self.column)] for j in
                         range(self.row)]
+        self.counter_ships = 20
         for i in range(self.row):
             for j in range(self.column):
                 btn = self.buttons[i][j]
-                btn.place(x=i * 29 + x, y=j * 26 + y) # Расположение каждого виджета на окне из перебора матрицы
+                btn.place(x=i * 29 + x, y=j * 26 + y)  # Расположение каждого виджета на окне из перебора матрицы
+        if player == 'human':
+            self.message_window = Label(tk, text='Выставите четыре однопалубных корабля', foreground="#B71C1C",
+                                        background="#FFCDD2", font=('Bauhaus 93', 18))
+            self.message_window.place(x=0, y=0)
+
+    @classmethod
+    def damage_bot(cls, category):
+        if not cls.unfinished:
+            while True:
+                button = choice(choice(cls.players[0].buttons)) if cls.button is None else cls.button
+                if button.shoot_down is False or button == cls.button:
+                    break
+        else:
+            button = choice(cls.unfinished)
+        while button.is_ship is True:
+            vessel = button.ship
+            if button.shoot_down is False:
+                vessel.lst_button.append(button)
+                cls.button = button
+                cls.unfinished.append(cls.button)
+                ship = button.ship
+                ship.count -= 1
+                cls.players[0].counter_ships -= 1
+                button.shoot_down = True
+                button.config(bg=ship.color, text='☠')
+                button['state'] = 'disable'
+                cls.game_state(category)
+                if ship.count == 0:
+                    for vd in ship.bt_lst:
+                        vd.config(bg='red', text='X', fg='#696969')
+                        cls.unfinished.remove(vd)
+                        cls.button = None
+                    if not cls.unfinished:
+                        while True:
+                            button = choice(choice(cls.players[0].buttons))
+                            if button.shoot_down is False:
+                                break
+                    else:
+                        button = choice(cls.unfinished)
+                    continue
+            cls.game_state(category)
+            if len(vessel.lst_button) == 1:
+                x = button.x
+                y = button.y
+                while True:
+                    button = choice(
+                        [cls.players[0].buttons[
+                             choice((x + 1, x - 1)) if 1 <= x <= 8 else (x + 1, x - 1)[0 if x <= 8 else 1]][y],
+                         cls.players[0].buttons[x][
+                             choice((y + 1, y - 1)) if 1 <= y <= 8 else (y + 1, y - 1)[0 if y <= 8 else 1]]])
+                    if button.shoot_down is False:
+                        break
+                continue
+            elif len(vessel.lst_button) == 2:
+                if vessel.lst_button[0].x == vessel.lst_button[1].x:  # Если равны по x
+                    obj = max(vessel.lst_button, key=lambda el: int(str(el).split()[1]))
+                    if obj.y < 9 and cls.players[0].buttons[obj.x][obj.y + 1] not in vessel.attempts \
+                            and cls.players[0].buttons[obj.x][obj.y + 1].shoot_down is False:
+                        button = cls.players[0].buttons[obj.x][obj.y + 1]
+                        vessel.attempts.add(button)
+                        continue
+                    elif obj.y > 1:
+                        button = cls.players[0].buttons[obj.x][obj.y - 2]
+                        vessel.attempts.add(button)
+                        continue
+                else:
+                    obj = max(vessel.lst_button, key=lambda el: int(str(el).split()[0]))
+                    if obj.x < 9 and cls.players[0].buttons[obj.x + 1][obj.y] not in vessel.attempts \
+                            and cls.players[0].buttons[obj.x + 1][obj.y].shoot_down is False:
+                        button = cls.players[0].buttons[obj.x + 1][obj.y]
+                        vessel.attempts.add(button)
+                        continue
+                    elif obj.x > 1 and cls.players[0].buttons[obj.x - 2][obj.y] not in vessel.attempts:
+                        button = cls.players[0].buttons[obj.x - 2][obj.y]
+                        vessel.attempts.add(button)
+                        continue
+            elif len(vessel.lst_button) == 3:
+                if vessel.lst_button[0].x == vessel.lst_button[1].x == vessel.lst_button[2].x:
+                    obj = max(vessel.lst_button, key=lambda el: int(str(el).split()[1]))
+                    if obj.y < 9 and cls.players[0].buttons[obj.x][obj.y + 1] not in vessel.attempts \
+                            and cls.players[0].buttons[obj.x][obj.y + 1].shoot_down is False:
+                        button = cls.players[0].buttons[obj.x][obj.y + 1]
+                        vessel.attempts.add(button)
+                        continue
+                    elif obj.y > 3 and cls.players[0].buttons[obj.x][obj.y - 3] not in vessel.attempts:
+                        button = cls.players[0].buttons[obj.x][obj.y - 3]
+                        vessel.attempts.add(button)
+                        continue
+                else:
+                    obj = max(vessel.lst_button, key=lambda el: int(str(el).split()[0]))
+                    if obj.x < 9 and cls.players[0].buttons[obj.x + 1][obj.y] not in vessel.attempts \
+                            and cls.players[0].buttons[obj.x + 1][obj.y].shoot_down is False:
+                        button = cls.players[0].buttons[obj.x + 1][obj.y]
+                        vessel.attempts.add(button)
+                        continue
+                    elif obj.x > 3 and cls.players[0].buttons[obj.x - 3][obj.y] not in vessel.attempts:
+                        button = cls.players[0].buttons[obj.x - 3][obj.y]
+                        vessel.attempts.add(button)
+                        continue
+        button.config(text='⊗')
+        button.shoot_down = True
+        button['state'] = 'disable'
+
+    @classmethod
+    def game_state(cls, category):
+        """Состояние игры"""
+        if cls.players[0].counter_ships == 0:  # Побеждает противник
+            for i in range(10):
+                for j in range(10):
+                    cls.players[0].buttons[i][j]['state'] = 'disable'
+                    cls.players[1].buttons[i][j]['state'] = 'disable'
+            category.Message_window.destroy()
+            cls.Message_window = Label(text='Вы проиграли!(ಥ﹏ಥ)', bg='#B22222', font=('Bauhaus 93', 48, "bold"))
+            cls.Message_window.place(x=180, y=0)
+            category.Message_window = None
+
+        elif cls.players[1].counter_ships == 0:  # Выигрывает пользаватель
+            for i in range(10):
+                for j in range(10):
+                    cls.players[0].buttons[i][j]['state'] = 'disable'
+                    cls.players[1].buttons[i][j]['state'] = 'disable'
+            category.Message_window.destroy()
+            cls.Message_window = Label(text='Вы победили!', bg='#B22222', font=("Bauhaus 93", 48, "bold"))
+            cls.Message_window.place(x=250, y=0)
+            category.Message_window = None
 
     def separation_bot(self):
-        pass
+        """Инициализация игрового поля ИИ"""
+        pole_ship = 20
+        Ships = [(Ship('double_deck', 2, 'orange'), Ship('double_deck', 2, 'orange'), Ship('double_deck', 2, 'orange')),
+                 (Ship('three_deck', 3, 'violet'), Ship('three_deck', 3, 'violet')),
+                 Ship('four_deck', 4, 'black')
+                 ]
+        for x in range(10):
+            for y in range(10):
+                band = self.buttons[randint(0, 9)][randint(0, 9)]
+                if pole_ship > 16 and band.is_ship is False:
+                    band.is_ship = True
+                    band.ship = Ship('single_deck', 1, '#8B4513')
+                    band.ship.bt_lst.append(band)
+                    pole_ship -= 1
+                    continue
+                elif pole_ship > 10 and band.is_ship is False:
+                    band.is_ship = True
+                    band.ship = Ships[0][0 if pole_ship > 14 else [1, 2][0 if pole_ship > 12 else 1]]
+                    band.ship.bt_lst.append(band)
+                    band_2 = choice(list(filter(None, (self[band.x - 1, band.y], self[band.x + 1, band.y],
+                                                       self[band.x, band.y - 1], self[band.x + 1, band.y]))))
+                    band_2.is_ship = True
+                    band_2.ship = Ships[0][0 if pole_ship > 14 else [1, 2][0 if pole_ship > 12 else 1]]
+                    band_2.ship.bt_lst.append(band_2)
+                    pole_ship -= 2
+                elif pole_ship > 4 and band.is_ship is False:
+                    if band.x <= 7 and self.buttons[band.x + 1][band.y].is_ship is False and \
+                            self.buttons[band.x + 2][band.y].is_ship is False:
+                        for ind in [band.x, band.x + 1, band.x + 2]:
+                            self.buttons[ind][band.y].is_ship = True
+                            self.buttons[ind][band.y].ship = Ships[1][0 if pole_ship > 7 else 1]
+                            self.buttons[ind][band.y].ship.bt_lst.append(self.buttons[ind][band.y])
+                        pole_ship -= 3
+                    elif band.x >= 2 and self.buttons[band.x - 1][band.y].is_ship is False and self.buttons[band.x - 2][
+                         band.y].is_ship is False:
+                        for ind in [band.x, band.x - 1, band.x - 2]:
+                            self.buttons[ind][band.y].is_ship = True
+                            self.buttons[ind][band.y].ship = Ships[1][0 if pole_ship > 7 else 1]
+                            self.buttons[ind][band.y].ship.bt_lst.append(self.buttons[ind][band.y])
+                        pole_ship -= 3
+                    elif band.y <= 7 and self.buttons[band.x][band.y + 1].is_ship is False and self.buttons[band.x][
+                         band.y + 2].is_ship is False:
+                        for ind in [band.y, band.y + 1, band.y + 2]:
+                            self.buttons[ind][band.y].is_ship = True
+                            self.buttons[ind][band.y].ship = Ships[1][0 if pole_ship > 7 else 1]
+                            self.buttons[ind][band.y].ship.bt_lst.append(self.buttons[ind][band.y])
+                        pole_ship -= 3
+                    elif band.y >= 2 and self.buttons[band.x][band.y - 1].is_ship is False and self.buttons[band.x][
+                         band.y - 2].is_ship is False:
+                        for ind in [band.y, band.y - 1, band.y - 2]:
+                            self.buttons[ind][band.y].is_ship = True
+                            self.buttons[ind][band.y].ship = Ships[1][0 if pole_ship > 7 else 1]
+                            self.buttons[ind][band.y].ship.bt_lst.append(self.buttons[ind][band.y])
+                        pole_ship -= 3
+                elif pole_ship > 1 and band.is_ship is False:
+                    if band.x <= 6 and self.buttons[band.x + 1][band.y].is_ship is False and \
+                            self.buttons[band.x + 2][band.y].is_ship is False \
+                            and self.buttons[band.x + 3][band.y].is_ship is False:
+                        for ind in [band.x, band.x + 1, band.x + 2, band.x + 3]:
+                            self.buttons[ind][band.y].is_ship = True
+                            self.buttons[ind][band.y].ship = Ships[2]
+                            self.buttons[ind][band.y].ship.bt_lst.append(self.buttons[ind][band.y])
+                        pole_ship -= 4
+                    elif band.x >= 3 and self.buttons[band.x - 1][band.y].is_ship is False and \
+                            self.buttons[band.x - 2][band.y].is_ship is False \
+                            and self.buttons[band.x - 3][band.y].is_ship is False:
+                        for ind in [band.x, band.x - 1, band.x - 2, band.x - 3]:
+                            self.buttons[ind][band.y].is_ship = True
+                            self.buttons[ind][band.y].ship = Ships[2]
+                            self.buttons[ind][band.y].ship.bt_lst.append(self.buttons[ind][band.y])
+                        pole_ship -= 4
+                    elif band.y <= 6 and self.buttons[band.x][band.y + 1].is_ship is False and self.buttons[band.x][
+                         band.y + 2].is_ship is False and self.buttons[band.x][band.y + 3].is_ship is False:
+                        for ind in [band.y, band.y + 1, band.y + 2, band.y + 3]:
+                            self.buttons[band.x][ind].is_ship = True
+                            self.buttons[band.x][ind].ship = Ships[2]
+                            self.buttons[band.x][ind].ship.bt_lst.append(self.buttons[band.x][ind])
+                        pole_ship -= 4
+                    elif band.y >= 3 and self.buttons[band.x][band.y - 1].is_ship is False and self.buttons[band.x][
+                         band.y - 2].is_ship is False and self.buttons[band.x][band.y - 3].is_ship is False:
+                        for ind in [band.y, band.y - 1, band.y - 2, band.y - 3]:
+                            self.buttons[band.x][ind].is_ship = True
+                            self.buttons[band.x][ind].ship = Ships[2]
+                            self.buttons[band.x][ind].ship.bt_lst.append(self.buttons[band.x][ind])
+                        pole_ship -= 4
+
+    def __getitem__(self, item):
+        if 0 <= item[0] <= 9 and 0 <= item[1] <= 9 and self.buttons[item[0]][item[1]].is_ship is False:
+            return self.buttons[item[0]][item[1]]
+        return None
 
 
 class Window:
@@ -37,20 +264,39 @@ class Window:
         tk = Tk()
         image = PhotoImage(file='123.png')
         Label(image=image).pack(fill='both')
+        Button(text='Начать заново', font=("Bauhaus 93", 20, "bold"),
+               command=lambda root=tk: self.restart(root)).place(x=0, y=650)
+        Button(text='Выйти', font=("Bauhaus 93", 20, "bold"),
+               command=tk.quit).place(x=890, y=650)
         tk.iconphoto(False, PhotoImage(file='unnamed.png'))
         tk.title('Морской бой')
         tk.geometry('1000x700')
         tk.resizable(False, False)
-        global message_window
-        message_window = Label(tk, text='Выставите четыре однопалубных корабля', font=('Bauhaus 93', 18))
-        message_window.place(x=0, y=0)
-        game_human = Game_pole(tk, 100, 200, 'human')
+        Game_pole(tk, 100, 200, 'human')
         game_bot = Game_pole(tk, 600, 200, 'bot')
+        game_bot.separation_bot()
         tk.mainloop()
+
+    @staticmethod
+    def restart(tk) -> None:
+        MyButton.init_pole_human = 20
+        MyButton.prev = None
+        if MyButton.Message_window:
+            MyButton.Message_window.destroy()
+        if Game_pole.Message_window:
+            Game_pole.Message_window.destroy()
+            Game_pole.Message_window = None
+        MyButton.Message_window = None
+        Game_pole.players = []
+        Game_pole.button = None
+        Game_pole.unfinished = []
+        Game_pole(tk, 100, 200, 'human')
+        game_bot = Game_pole(tk, 600, 200, 'bot')
+        game_bot.separation_bot()
 
 
 def start():
-    play = Window()
+    Window()
 
 
 if __name__ == '__main__':
