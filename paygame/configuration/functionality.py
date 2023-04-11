d527ecd26b2ec214375c8e5bbe66e89384ad663f
Bogdan <123867305+BgPyt@users.noreply.github.com>
2023-04-11 16:45:23 +0700

    Update functionality.py

--- paygame/configuration/functionality.py
+++ paygame/configuration/functionality.py
@@ -1,4 +1,4 @@
-from paygame.configuration.initialization.py import Ship, Label
+from paygame.configuration.initialization import Ship, Label
 import tkinter.messagebox as mb
 from tkinter import Button
 import time
